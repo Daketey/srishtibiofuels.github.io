e4bcbcb31799b4fb6a2e1f0329d7c8d386193da2
daketey <ritul.mahapatra@gmail.com>
2025-11-06 00:31:50 +0530

    website build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   server: {
     allowedHosts: [
       'd1edbfe22567.ngrok-free.app',
